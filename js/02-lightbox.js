871c24ccf28a4e16b2586fca335d31190cfc54f1
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-18 23:19:37 +0200

    Add solution 02 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const galleryListItems = createGaleryItem(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryListItems);
+
+function createGaleryItem(galleryItems) {
+  const galleryItem = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li> `;
+    })
+    .join("");
+
+  return galleryItem;
+}
+
+var lightbox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
